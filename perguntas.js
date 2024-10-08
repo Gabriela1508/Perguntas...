d9ae90ce65958471007a2c358ec004b93e90237e
Gabriela1508 <135843036+Gabriela1508@users.noreply.github.com>
2024-10-08 12:12:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criaCartao(
+    'Química',
+    'Qual a diferença entre a ligação covalente e iônica?',
+    'ligação covalente é quando 2 atomos compartilham eletrons já na iônica um atomo doa eletrons para outro atomo'
+)
+
+criaCartao(
+    'Biologia',
+    'O que era seleção natural proposto por Charles Dawin?',
+    'a seleção natural para darwin vinha da suposição que o ser que mais se adapta é o que sobrevive'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Índia?',
+    'Nova Delhi'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor país do Mundo?',
+    'Vaticano'
+)
+
+criaCartao(
+'matemática',
+'Qual valor de π (Pi)?',
+'3,141592653...'
+)
+
+criaCartao(
+    'Matemática',
+    'O que o filho do matemático fala quando quer ir ao banheiro?',
+    'Pi-Pi'
+)
+
+criaCartao(
+    'Química',
+    'O que significa densidade?',
+    'É quanto determinada massa de um elemento ocupa certo espaço.'
+)
+
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico.'
+)
+
+criaCartao(
+    'História',
+    'Qual Presidente do Brasil conhecido como Jango?',
+    'João Goulart.',
+)
+
+criaCartao(
+    'Artes',
+    "A pintura mais famosa de Pablo Picasso ?",
+    'La Guernica.',
+)
+
+criaCartao(
+'Química',
+'Quantos elementos existem na tabela periodica?',
+'118 Elementos.',
+)
+
+criaCartao(
+'História',
+'Quem foi o Primeiro Presidente do Brasil ?',
+'Merechal Manuel Deodoro da Fonseca.',
+)
+
+criaCartao(
+'Artes',
+'Por que van gogh pintou Noite estrelado?',
+'Foi uma tentativa de expressar um estado de choque',
+
+)
+
+criaCartao(
+    'História',
+    'Quantos anos tem o Brasil ?',
+    '524 Anos de Descobrimento.'
+)
+
+criaCartao(
+    'Charada',
+    'Eu pertenço ao mês de Dezembro, e não a qualquer outro mês. Eu não sou um feriado. Quem eu sou ?',
+    'As letras "D" e "Z" porque nenhum outro mês tem essas letras',
+)
+
+criaCartao(
+    'Charada',
+    'O que pode correr, mais nunca anda; tem leito, mais nuca dorme; nasce, mais não morre ?',
+    'O rio',
+)
+
+criaCartao(
+'Matemática',
+'Um pequeno caminhão pode carregar 50 sacos de areia ou 400 tijolos. Se foram colocados no caminhão 32 sacos de areia, quantos tijolos pode ainda ele carregar?',
+'144 tijolos, porque: 1 saco de areia = 8 tijolos. Se o caminhão carregou 32 sacos de areia, ainda tem espaço para 18 sacos, mas em vez de sacos, quer levar tijolos, ou seja 18 . 8 = 144.',
+)
